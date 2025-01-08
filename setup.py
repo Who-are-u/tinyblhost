b40d8c2a55c49f3a615c54bfa2ebb5374a41e7ce
Jianping Zhang <jianping.zhang_2@nxp.com>
2025-01-08 17:47:02 +0800

    add docs, examples, rename test to tests, update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='tinyblhost',
     version='0.1',
-    packages=find_packages(),
+    packages=find_packages(
+        exclude=["tests", "docs", "examples"],
+    ),
+    install_requires = ['pyserial', 'crcmod']
 )
